1469a99aff3d4fd81b379b0cb403c0f43de26f4d
jonfreibr <jfreivald@brmedical.com>
2025-02-10 15:03:43 -0500

    Update install.bat
    No more hard-coding paths -- will source install from anywhere.

--- install.bat
+++ install.bat
@@ -1,15 +1,14 @@
 @echo off
-h:
-cd \_BRMCApps\WeatherAlerts
+echo Sourcing from: %~dp0
 echo Installing Python 3.11.5
-python-3.11.5-amd64.exe /passive
+%~dp0\python-3.11.5-amd64.exe /passive
 echo upgrading pip
 %LocalAppData%\Programs\Python\Python311\python.exe -m pip install --upgrade pip -q
 echo Adding package requients
 %LocalAppData%\Programs\Python\Python311\Scripts\pip.exe install -r requirements.txt -q
 echo Copying files
 if not exist %USERPROFILE%\Walerts md %USERPROFILE%\Walerts
-copy /y walerts.py %USERPROFILE%\Walerts
-copy /y "Weather Alerts.lnk" %USERPROFILE%\Desktop
+copy /y %~dp0\walerts.py %USERPROFILE%\Walerts
+copy /y "%~dp0\Weather Alerts.lnk" %USERPROFILE%\Desktop
 echo Done with installation
 start %USERPROFILE%"\Desktop\Weather Alerts.lnk"

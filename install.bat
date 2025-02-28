f59f30589dd46e61eed4ab834c9fb35ec7fa1a9e
jonfreibr <jfreivald@brmedical.com>
2025-02-28 10:19:15 -0500

    PySimpleGUI install changes

--- install.bat
+++ install.bat
@@ -6,6 +6,7 @@ echo upgrading pip
 %LocalAppData%\Programs\Python\Python311\python.exe -m pip install --upgrade pip -q
 echo Adding package requients
 %LocalAppData%\Programs\Python\Python311\Scripts\pip.exe install -r %~dp0\requirements.txt -q
+%LocalAppData%\Programs\Python\Python311\Scripts\pip.exe install -i https://PySimpleGUI.net/install PySimpleGUI -q
 echo Copying files
 if not exist %USERPROFILE%\Walerts md %USERPROFILE%\Walerts
 copy /y %~dp0\walerts.py %USERPROFILE%\Walerts

2089534dc113a3b682d7e5848505dffe474f762c
jonfreibr <jfreivald@brmedical.com>
2025-07-24 09:09:53 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -3,9 +3,9 @@ echo Sourcing from: %~dp0
 echo Installing Python 3.11.5
 "%~dp0\python-3.11.5-amd64.exe" /passive
 echo upgrading pip
-%LocalAppData%\Programs\Python\Python311\python.exe -m pip install --upgrade pip -q
+%LocalAppData%\Programs\Python\Python311\python.exe -m pip install --upgrade pip
 echo Adding package requirements
-%LocalAppData%\Programs\Python\Python311\Scripts\pip.exe install -r "%~dp0\requirements.txt" -q
+%LocalAppData%\Programs\Python\Python311\Scripts\pip.exe install -r "%~dp0\requirements.txt"
 echo Copying files
 if not exist %USERPROFILE%\Walerts md %USERPROFILE%\Walerts
 copy /y "%~dp0\walerts.py" %USERPROFILE%\Walerts

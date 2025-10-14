6db139b10b74377ca3a3c06dd0588f49f6eea1fc
jonfreibr <jfreivald@brmedical.com>
2025-10-14 10:37:21 -0400

    Update walerts.py

--- walerts.py
+++ walerts.py
@@ -119,7 +119,7 @@ class UpdateDialog(QDialog):
 def is_running(script):
     for q in psutil.process_iter():
         if q.name().startswith('python'):
-            if len(q.cmdline())>1 and script in q.cmdline()[1] and q.pid !=os.getpid():
+            if len(q.cmdline())>1 and script in q.cmdline()[1] and q.pid != os.getpid():
                 # print(f"'{script}' Process is already running")
                 return True
             
